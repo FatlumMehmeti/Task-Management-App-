ebe24a12a086d4c56d089edd2de65705263192b9
armend24 <armendbytyqi242003@gmail.com>
2025-11-03 14:36:51 +0100

    Dashboard: Scaffold initial directory structure

---
+++ src/DashboardLayout/DashboardLayout.tsx
@@ -0,0 +1,3 @@
+// function DashboardLayout(): JSX.Element {
+//   return <Sidebar />;
+// }
---
+++ src/DashboardLayout/Sidebar/Sidebar.tsx
@@ -0,0 +1,10 @@
+// export default function Sidebar(): JSX.Element {
+//   return (
+//     <div className="sidebar">
+//       <button> Boards </button>
+//       <button> Templates </button>
+//       <button> Home </button>
+//       <hr />
+//     </div>
+//   );
+// }
