c20c858c7062f9e93c4a0b5d82bc869b246283ef
armend24 <armendbytyqi242003@gmail.com>
2025-11-03 13:40:21 +0100

    Add Navbar component and wire it into App

---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      {/* navbar-LEFT */}
+      <div className="navbar-left">Trello</div>
+
+      {/* navbar-MIDDLE */}
+      <div className="navbar-middle">
+        <input className="search-bar" type="text" placeholder="Search" />
+        <button className="create-button">Create</button>
+      </div>
+
+      {/* navbar-RIGHT */}
+      <div className="navbar-right">
+        <button className="notifications-button navbar-right-button"></button>
+        <button className="channel-button navbar-right-button"> </button>
+        <button className="information-button navbar-right-button"> </button>
+        <button className="profile-button navbar-right-button"> </button>
+      </div>
+    </div>
+  );
+}
