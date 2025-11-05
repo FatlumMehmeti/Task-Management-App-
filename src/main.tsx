b39d3a1f8e7f1eb400716deae840c52f5dc6bcb4
Florent <florent.sylejmani@student.uni-pr.edu>
2025-11-05 10:32:23 +0100

    fix(main): resolve merge conflicts and clean up imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import "./styles/index.css";
-import App from './App.tsx'
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppRouter } from "./router";
 import { Toaster } from "sonner";
 import "./index.css";
->>>>>>> main
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
